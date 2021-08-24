1566e6dfb366fd34e67cba62156b60a64f4fb882
aoito <ao130324@gmail.com>
2021-08-24 17:50:23 +0900

    flex

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -3,17 +3,17 @@
         <div class="container mt-5 px-5">
             <h1 class="mb-4">INTRODUCTION</h1>
                 <div class="me">
-                    <h2><font-awesome-icon icon="pen" class="icon"/> Name</h2>
+                    <h2><font-awesome-icon icon="pen" class="icon"/>Name</h2>
                     <p class="text">Ao Ito</p>
                 </div>
 
                 <div class="me">
-                    <h2><font-awesome-icon icon="school" class="icon"/> School</h2>
+                    <h2><font-awesome-icon icon="school" class="icon"/>School</h2>
                     <p class="text">Musashi Univercity</p>
                 </div>
 
                 <div class="me">
-                    <h2><font-awesome-icon icon="birthday-cake" class="icon"/> Birthday</h2>
+                    <h2><font-awesome-icon icon="birthday-cake" class="icon"/>Birthday</h2>
                     <p class="text">2001-03-24</p>
               </div>
         </div>
